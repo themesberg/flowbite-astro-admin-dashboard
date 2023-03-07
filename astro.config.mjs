c396c97b03a09844429a483039d2a5792dfd8137
Julian Cataldo <contact@juliancataldo.com>
2023-03-07 21:15:01 +0100

    ci: fix invalid trailing slash option 🤦🏻‍♂️

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
 	site: 'https://themesberg.github.io/flowbite-astro-admin-dashboard',
 
 	/* Like Vercel, Netlify, GH Pages… Mimicking for dev. server */
-	trailingSlash: true,
+	trailingSlash: 'always',
 
 	server: {
 		/* Dev. server only */
