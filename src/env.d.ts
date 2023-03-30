fb3dadda3c956aab6a8b5dfda9d40ce1445f03af
Julian Cataldo <contact@juliancataldo.com>
2023-03-30 08:13:34 +0200

    feat: dynamicize charts, flowbite ui, github
    + Add SITE env to ambient

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+// https://docs.astro.build/en/guides/environment-variables/#intellisense-for-typescript
+interface ImportMetaEnv {
+	readonly SITE: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
